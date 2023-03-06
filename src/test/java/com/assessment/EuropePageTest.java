40d352b6a4a1d3dc3fe1da86d54ea69a2cd56d97
dharanikesav <dharanikesav@gmail.com>
2023-03-06 16:25:33 +0100

    Parameterize download brochure test

--- src/test/java/com/assessment/EuropePageTest.java
+++ src/test/java/com/assessment/EuropePageTest.java
@@ -37,11 +37,26 @@ public class EuropePageTest extends BaseTest {
         try {
             EuropeToursPage europeToursPage = new EuropeToursPage();
             europeToursPage.navigate();
-            europeToursPage.downloadBrochure("dharanikesav@gmail.com");
+            String email = String.valueOf(System.getProperty("Email"));
+            if(isNotEmptyOrNull("Email", email)) {
+                europeToursPage.downloadBrochure(email);
+            }
+
         } catch (Exception e) {
             ExtentReport.reportError(e);
             ExtentReport.flushReport();
             throw e;
         }
+    }
+
+    private boolean isNotEmptyOrNull(String argName, String argValue) {
+        if(String.valueOf(argValue).equalsIgnoreCase("null")) {
+            throw new CustomException("no value is passed argument :'" + argName + "'. please provide value");
+        }
+
+        if(argValue.isEmpty()) {
+            throw new CustomException("Value for argument :'" + argName + "' should not be blank. please provide correct value");
+        }
+        return true;
     }
 }
